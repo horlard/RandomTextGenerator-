fc45e4408c0be93f064a5b4812514eab14e255c5
horlard <oladipuposaulih@gmail.com>
2019-06-06 19:03:31 +0100

    imported axios into the app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
-
+import axios from 'axios';
 class App extends React.Component{
+    state = {
+        paras: 4,
+        type: 'hipster-latin',
+        html: true
+    }
+    componentDidMount(){
+        async const response =  await axios.get('http://hipsterjesus.com/api/',{
+            params : {
+                paras : this.state.paras,
+                type: this.state.type,
+                html: this.state.html
+
+            }
+        })
+        console.log(response);
+    }
+
     render() {
-        return <div>App</div>
+        return <div></div>
     }
 }
 export default App;
