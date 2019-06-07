63b657e0e052206622c72628aaa499050bb4f567
horlard <oladipuposaulih@gmail.com>
2019-06-07 23:09:04 +0100

    created an helper method to handle conditions for the format param

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import TextDetail from './text-details';
+import '../style.css'
 class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.inputRef= React.createRef()
+        this.OpRef= React.createRef();
+
+        
+    }
     state = {
-        paras: 4,
         type: 'all meat',
-        format: 'text',
         text: ''
     }
-    componentDidMount= async () =>{
+   
+    onConvert = async () => {
          const response =  await axios.get('https://baconipsum.com/api/',{
             params : {
                 type: this.state.type,
-                paras : this.state.paras,
-                format : this.state.format
+                paras : this.inputRef.value,
+                format : this.onFormat()
                 
                 
 
@@ -21,12 +29,48 @@ class App extends React.Component{
         }
         
         )
-        console.log(response.data);
+        
+        this.setState({text: response.data})
+        
+        
         
     }
 
+    onFormat = ()=> {
+        if(this.OpRef.value ==='html') {
+            return 'html';
+        }else{
+            return 'text';
+        }
+    }
+
     render() {
-        return <div></div>
+        console.log(this.OpRef.value)
+        return (
+            <div className='ui container'>
+                <h1 className='ui header'>
+                Random text generator
+            </h1>
+            <div>
+                <span className='ui header'>Format:</span>
+            <select className="ui dropdown" ref={OpRef=>this.OpRef=OpRef}>
+                <option  >html</option>
+                <option >Text</option>
+            </select>
+            <span className='ui header'>Paragraph:</span>
+            <div className="ui input">
+                
+                <input type="number" placeholder="0" ref={inputRef => this.inputRef = inputRef}/>
+            </div>
+                <button className="ui primary button" onClick={this.onConvert}>
+                    Generate
+                </button>
+            </div>
+                <TextDetail  text={this.state.text}/>
+            
+            </div>
+            
+        )
     }
 }
 export default App;
