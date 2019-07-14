567f249a87f8f597779135ed27d85412ecd95b9a
horlard <oladipuposaulih@gmail.com>
2019-07-14 16:57:27 +0100

    jibberish mistakes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
 import TextDetail from './text-details';
-import Loader from './loader';
 import '../style.css'
 
 class App extends React.Component{
